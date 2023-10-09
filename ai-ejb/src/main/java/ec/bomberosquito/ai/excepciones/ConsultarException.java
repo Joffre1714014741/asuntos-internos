0d182e54a7bfeaa59d15c7551174db1fcb1c4ba3
Samuel Hwang <shwang@bomberosquito.gob.ec>
2023-10-09 12:22:35 -0500

    Función para realizar consultas desde las fachadas

---
+++ ai-ejb/src/main/java/ec/bomberosquito/ai/excepciones/ConsultarException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.bomberosquito.ai.excepciones;
+
+/**
+ *
+ * @author danielhwang
+ */
+public class ConsultarException extends Exception {
+
+    public ConsultarException(String message, Throwable cause) {
+        super("Error al ejecutar la consulta :\n" + message, cause);
+    }
+
+    public ConsultarException(String message) {
+        super("Error al ejecutar la consulta :\n" + message);
+    }
+
+}
--- ai-ejb/src/main/java/ec/bomberosquito/ai/facades/AbstractFacade.java
+++ ai-ejb/src/main/java/ec/bomberosquito/ai/facades/AbstractFacade.java
@@ -4,7 +4,11 @@
  */
 package ec.bomberosquito.ai.facades;
 
+import ec.bomberosquito.ai.excepciones.ConsultarException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.EntityManager;
 
 /**
@@ -58,6 +62,109 @@ public abstract class AbstractFacade<T> {
         cq.select(getEntityManager().getCriteriaBuilder().count(rt));
         javax.persistence.Query q = getEntityManager().createQuery(cq);
         return ((Long) q.getSingleResult()).intValue();
+    }
+    
+    public List<T> encontrarParametros(Map parametros) throws ConsultarException {
+        try {
+            Iterator it = parametros.entrySet().iterator();
+            String where = "";
+            String orden = "";
+            boolean all = false;
+            int maxResults = -1;
+            int firstResult = -1;
+            String tabla = entityClass.getSimpleName();
+            Map par = new HashMap();
+            while (it.hasNext()) {
+                Map.Entry e = (Map.Entry) it.next();
+                String clave = (String) e.getKey();
+                if (clave.contains(";where")) {
+                    where = (String) e.getValue();
+                } else if (clave.contains(";inicial")) {
+                    firstResult = (Integer) e.getValue();
+                    all = true;
+                } else if (clave.contains(";orden")) {
+                    orden = " order by " + (String) e.getValue();
+
+                } else if (clave.contains(";final")) {
+                    all = true;
+                    maxResults = (Integer) e.getValue();
+                } else {
+                    par.put(clave, e.getValue());
+                }
+
+            }
+            if (!where.isEmpty()) {
+                where = " where " + where;
+            }
+
+            String sql = "Select object(o) from " + tabla + " as o " + where + orden;
+            javax.persistence.Query q = getEntityManager().createQuery(sql);
+
+            it = par.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry e = (Map.Entry) it.next();
+                String clave = (String) e.getKey();
+
+                q.setParameter(clave, e.getValue());
+            }
+            if (all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } catch (Exception e) {
+            throw new ConsultarException(entityClass.toString(), e);
+        }
+    }
+
+    public int contar(Map parametros) throws ConsultarException {
+        try {
+            Iterator it = parametros.entrySet().iterator();
+            String where = "";
+            String orden = "";
+            boolean all = false;
+            int maxResults = -1;
+            int firstResult = -1;
+            String tabla = entityClass.getSimpleName();
+            Map par = new HashMap();
+            while (it.hasNext()) {
+                Map.Entry e = (Map.Entry) it.next();
+                String clave = (String) e.getKey();
+                if (clave.contains("where")) {
+                    where = (String) e.getValue();
+                } else if (clave.contains(";inicial")) {
+                    firstResult = (Integer) e.getValue();
+                    all = true;
+                } else if (clave.contains(";orden")) {
+                    orden = " order by " + (String) e.getValue();
+
+                } else if (clave.contains(";final")) {
+                    all = true;
+                    maxResults = (Integer) e.getValue();
+                } else {
+                    par.put(clave, e.getValue());
+                }
+
+            }
+            if (!where.isEmpty()) {
+                where = " where " + where;
+            }
+
+            String sql = "Select count(o) from " + tabla + " as o " + where;
+            javax.persistence.Query q = getEntityManager().createQuery(sql);
+
+            it = par.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry e = (Map.Entry) it.next();
+                String clave = (String) e.getKey();
+
+                q.setParameter(clave, e.getValue());
+            }
+
+            return ((Long) q.getSingleResult()).intValue();
+        } catch (Exception e) {
+            throw new ConsultarException(entityClass.toString(), e);
+        }
     }
     
 }
